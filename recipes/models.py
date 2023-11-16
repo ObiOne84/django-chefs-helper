370b76d8c36f92868175ed34264b7f23aa6c3b38
ObiOne84 <stepienszymon@hotmail.com>
2023-11-16 20:12:20 +0000

    Add Recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipes")
+    created_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    instructions = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    likes = models.ManyToManyField(User, related_name="liked_recipes")
+    prep_time = models.IntegerField()
+    cook_time = models.IntegerField()
+    servings = models.IntegerField(default=1)
+    rating = models.IntegerField(default=0)
+    total_ratings = models.IntegerField(default=0)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-title']
+    
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+    def average_rating(self):
+        return self.rating
