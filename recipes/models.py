049197dd71e6faa653d215f9d369b6ce3a329636
ObiOne84 <stepienszymon@hotmail.com>
2023-11-16 20:18:15 +0000

    Add Review class model

--- recipes/models.py
+++ recipes/models.py
@@ -32,5 +32,32 @@ class Recipe(models.Model):
     def number_of_likes(self):
         return self.likes.count()
 
+    def number_of_reviews(self):
+        return self.reviews.count()
+
+    def rate_recipe(self, user, rating):
+        # ensuring the rating is between 1 and 5
+        rating = max(1, min(5, rating))
+        # Update the rating and total_ratings
+        self.rating = (self.rating * self.total_ratings + rating) // (self.total_ratings + 1)
+        self.total_ratings += 1
+        # save the changes
+        self.save()
+
     def average_rating(self):
         return self.rating
+
+
+class Review(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='reviews')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Review {self.body} by {self.name}"
