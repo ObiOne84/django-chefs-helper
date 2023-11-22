5e14335b9ca0b065e23e88d4bd80ac3afcfece6e
ObiOne84 <stepienszymon@hotmail.com>
2023-11-22 22:10:23 +0000

    Add basic structure for recipe detail page

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipe
 
 
@@ -9,3 +9,25 @@ class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class RecipeDetails(View):
+    def get(self, request, slug, *arg, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        reviews = recipe.reviews.filter(approved=True).order_by('created_on')
+        ingredients = recipe.ingredients
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "reviews": reviews,
+                "liked": liked,
+                "ingredients": ingredients,
+            },
+        )
