e0ee4aec6a8c78ef8b9aa7635185f7a90608c223
ObiOne84 <stepienszymon@hotmail.com>
2023-11-24 21:20:46 +0000

    Add print option to recipe page

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,8 @@
+// update html page date
 $('#copyright').text(new Date().getFullYear());
+
+// print recipe card
+$('#print-button').on("click", function () {
+    let body = $('body').html();
+    print(body);
+});
