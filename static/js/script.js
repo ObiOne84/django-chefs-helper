940e93b1a92f913d2091d789ffb15c32832aacdd
ObiOne84 <stepienszymon@hotmail.com>
2023-11-26 14:48:10 +0000

    Add timeout setting for Django messages

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,21 @@
-// update html page date
-$('#copyright').text(new Date().getFullYear());
+$(document).ready(function () {
+    // update html page date
+    $('#copyright').text(new Date().getFullYear());
 
-// print recipe card
-$('#print-button').on("click", function () {
-    let body = $('body').html();
-    print(body);
+    // print recipe card
+    $('#print-button').on("click", function () {
+        let body = $('body').html();
+        print(body);
+    });
+
+    // activate logout modal
+    $('#logout-link').on("click", function () {
+        let myModal = new bootstrap.Modal(document.getElementById('logoutModal'));
+        myModal.show();
+    })
+
+    // django messages timeout source:https://api.jquery.com/hide/#hide
+    setTimeout(function() {
+        $('#msg').hide();
+    }, 3000);
 });
-
-// activate logout modal
-$('#logout-link').on("click", function () {
-    let myModal = new bootstrap.Modal(document.getElementById('logoutModal'));
-    myModal.show();
-})
