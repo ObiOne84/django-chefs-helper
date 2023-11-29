ea6ad8a1cc8e5b9a446c658b49b67372c47dc69d
ObiOne84 <stepienszymon@hotmail.com>
2023-11-29 21:19:09 +0000

    Add validation to time and serving input

--- recipes/forms.py
+++ recipes/forms.py
@@ -25,3 +25,34 @@ class AddRecipeForm(forms.ModelForm):
             'servings',
             'status',
             ]
+        # instructions = forms.CharField(widget=forms.Textarea(attrs={'class': 'instructions-input'}))
+    prep_time = forms.IntegerField(
+        widget=forms.NumberInput(attrs={'class': 'positive-number'}),
+        min_value=0
+    )
+    cook_time = forms.IntegerField(
+        widget=forms.NumberInput(attrs={'class': 'positive-number'}),
+        min_value=0
+    )
+    servings = forms.IntegerField(
+        widget=forms.NumberInput(attrs={'class': 'positive-number'}),
+        min_value=0
+    )
+
+    def clean_prep_time(self):
+        prep_time = self.cleaned_data['prep_time']
+        if prep_time < 0:
+            raise forms.ValidationError("Prep time must be a positive number.")
+        return prep_time
+
+    def clean_cook_time(self):
+        cook_time = self.cleaned_data['cook_time']
+        if cook_time < 0:
+            raise forms.ValidationError("Cook time must be a positive number.")
+        return cook_time
+
+    def clean_servings(self):
+        servings = self.cleaned_data['servings']
+        if servings < 0:
+            raise forms.ValidationError("Servings must be a positive number.")
+        return servings
